4bac1ea13cfe24c33b9f29b5120d64bf7c91a862
Adam Walsh <adamtwalsh@gmail.com>
2018-08-31 00:35:37 -0400

    update config

--- img2s3/config.py
+++ img2s3/config.py
@@ -1,4 +1,5 @@
 import os
+
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
@@ -8,52 +9,16 @@ postegres_uri_template = 'postgresql://{}:{}@{}:{}/{}'
 class Config:
     def __init__(self):
         self.SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-class DevelopmentConfig(Config):
-    def __init__(self):
-        super(DevelopmentConfig, self).__init__()
         self.DEBUG = True
         self.ENV = 'development'
-        self.SQLALCHEMY_DATABASE_URI = postegres_uri_template.format(
-            os.environ['DEV_DB_USERNAME'],
-            os.environ['DEV_DB_PASSWORD'],
-            os.environ['DEV_DB_ADDRESS'],
-            os.environ['DEV_DB_PORT'],
-            os.environ['DEV_DB_NAME']
-        )
-
-
-class TestConfig(Config):
-    def __init__(self):
-        super(TestConfig, self).__init__()
-        self.TESTING = True
-        self.ENV = 'development'
-        self.SQLALCHEMY_DATABASE_URI = postegres_uri_template.format(
-            os.environ['TEST_DB_USERNAME'],
-            os.environ['TEST_DB_PASSWORD'],
-            os.environ['TEST_DB_ADDRESS'],
-            os.environ['TEST_DB_PORT'],
-            os.environ['TEST_DB_NAME']
-        )
-
-
-class ProductionConfig(Config):
-    def __init__(self):
-        super(ProductionConfig, self).__init__()
-        self.SQLALCHEMY_DATABASE_URI = postegres_uri_template.format(
-            os.environ['DB_USERNAME'],
-            os.environ['DB_PASSWORD'],
-            os.environ['DB_ADDRESS'],
-            os.environ['DB_PORT'],
-            os.environ['DB_NAME']
-        )
+        self.ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+        # S3 image bucket
+        self.image_s3_bucket = os.environ['S3_BUCKET']
+        self.image_s3_bucket_key_id = os.environ['S3_KEY_ID']
+        self.image_s3_bucket_access_key = os.environ['S3_ACCESS_SECRET']
+        self.image_s3_bucket_location = f'https://s3.amazonaws.com/{self.image_s3_bucket}'
+        self.image_s3_bucket_folder = os.environ.get('S3_BUCKET_DIR')
 
 
 def create_config() -> Config:
-    if os.getenv('TEST') == '1' or os.getenv('TRAVIS') == 'true':
-        return TestConfig()
-    elif os.getenv('DEV') == '1':
-        return DevelopmentConfig()
-    else:
-        return ProductionConfig()
+    return Config()
